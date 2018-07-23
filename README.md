da01c3ee2f2a981f8f64877a679b863c9e81bbec
Kyle Seifert <seifert.kyle@gmail.com>
2018-07-23 10:19:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# dotfiles
+
+Use GNU stow to manage these dotfiles. 
+
+Clone this project into home directory, then
+```
+cd ~/dotfiles
+stow bash
+stow tilda
+```
+etc for the configuration to install. GNU stow symlinks each config to the appropriate place.
---
+++ psysh/.config/psysh/config.php
@@ -0,0 +1,9 @@
+<?php
+$defaultIncludes = [];
+$composerAutoload = getcwd() . DIRECTORY_SEPARATOR . '/vendor/autoload.php';
+if (is_file($composerAutoload)) {
+    $defaultIncludes[] = $composerAutoload;
+}
+return [
+    'defaultIncludes' => $defaultIncludes,
+];
